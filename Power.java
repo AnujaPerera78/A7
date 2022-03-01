66723afd63b4238ab56c4687f7bea105785c073b
AnujaPerera78 <79879050+AnujaPerera78@users.noreply.github.com>
2022-02-28 22:48:43 -0500

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,90 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
+
+public class Power extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textBase;
+	private JTextField textExpoenent;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Power frame = new Power();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Power() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 477, 454);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Power");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(198, 11, 109, 31);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblEnterBase = new JLabel("Enter the Base");
+		lblEnterBase.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblEnterBase.setBounds(43, 58, 98, 31);
+		contentPane.add(lblEnterBase);
+		
+		JLabel lblEnterExp = new JLabel("Enter the Exponent ");
+		lblEnterExp.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblEnterExp.setBounds(43, 90, 127, 31);
+		contentPane.add(lblEnterExp);
+		
+		textBase = new JTextField();
+		textBase.setBounds(180, 63, 86, 20);
+		contentPane.add(textBase);
+		textBase.setColumns(10);
+		
+		textExpoenent = new JTextField();
+		textExpoenent.setColumns(10);
+		textExpoenent.setBounds(180, 97, 86, 20);
+		contentPane.add(textExpoenent);
+		
+		JButton btnCalculate = new JButton("Calculate");
+		btnCalculate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCalculate.setBounds(302, 62, 89, 23);
+		contentPane.add(btnCalculate);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.setBounds(302, 96, 89, 23);
+		contentPane.add(btnExit);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(43, 145, 358, 243);
+		contentPane.add(textArea);
+	}
+}
